6c3ce024aff492272efe9bb3247162540fa60604
moiz786 <moiz.786@hotmail.com>
2015-09-02 15:07:01 +0500

    Fixed issues.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,4 @@
 class ArticlesController < ApplicationController
-
-   http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
  
   def show
     @article = Article.find(params[:id])
--- db/schema.rb
+++ db/schema.rb
@@ -30,24 +30,6 @@ ActiveRecord::Schema.define(version: 20150826114935) do
 
   add_index "comments", ["article_id"], name: "index_comments_on_article_id"
 
-  create_table "models", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "models", ["email"], name: "index_models_on_email", unique: true
-  add_index "models", ["reset_password_token"], name: "index_models_on_reset_password_token", unique: true
-
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
